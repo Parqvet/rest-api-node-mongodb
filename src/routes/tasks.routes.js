21ceab620a0afb722e316219646864ee5b2a4a22
Marcelo <marcelosen4@gmail.com>
2020-12-10 12:50:30 -0300

    agregado metodo para buscar tarea en db por id

--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -4,6 +4,9 @@ const router = Router();
 // database connection
 import { connect } from '../database';
 
+// funcion para convertir un dato
+import { ObjectID } from 'mongodb';
+
 router.get('/', async (req, res) => {
     // antes de enviar una respuesta me voy a conectar a la db
     // esto me va a devolver una conexion a la base de datos
@@ -27,7 +30,18 @@ router.post('/', async (req, res) => {
     // una vez guardado los datos, genero una consulta a la db
     // antes de crear algo le tenemos que decir a que coleccion se lo vamos a asignar, luego guardamos la tarea
     const result = await db.collection('tasks').insertOne(task);
+    // para obtener solo los datos que se ha insertado, le decimos que queremos acceder a la propiedad ops, a su arreglo en su indice cero
     res.json(result.ops[0]);
+})
+
+router.get('/:id', async (req, res) => {
+    const { id } = req.params;
+    const db = await connect();
+
+    // db consulta a la coleccion tasks y busca un solo dato por el id
+    // mongodb consulta a un objeto y no a un string, por ende el id lo tenemos que pasar a objeto
+    const result = await db.collection('tasks').findOne({_id: ObjectID(id)});
+    res.json(result)
 })
 
 export default router;
